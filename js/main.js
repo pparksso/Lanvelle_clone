ad757350dd585c53eea5fbc5e7d9aefa46172d2c
pparksso <thgml5143@hanmail.net>
2022-07-28 16:43:06 +0900

    contents end

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const tabs = document.querySelectorAll("#contents .tabs ul li");
 const pannels = document.querySelectorAll("#contents .pannel");
 const timeSaleArea = document.querySelector("#timesale");
-const newBestArea = document.querySelector("#newBest");
-const promotionArea = document.querySelector("#promotion");
+const newBestArea = document.querySelector("#newBest ul");
+const promotionArea = document.querySelector("#promotion ul");
 const saleImgBox = document.querySelector("#timesale .saleImgBox");
 const timerArea = document.querySelector("#timesale .timer");
 const saleTxtBox = document.querySelector("#timesale .txtBox");
@@ -40,12 +40,14 @@ tabs.forEach((item, idx) => {
 //타임세일구역 타이머생성함수
 function timerMaker() {
   const today = new Date();
+  const todaySec = Math.trunc(today.getTime() / 1000);
   const tomorrow = new Date(today.setDate(today.getDate() + 1));
   const outTime = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 13, 0, 0);
-  const leftTime = outTime - today;
-  const hour = Math.floor(leftTime / (1000 * 60 * 60));
-  const min = Math.floor((leftTime % (1000 * 60 * 60)) / (1000 * 60));
-  const sec = Math.floor(((leftTime % (1000 * 60 * 60)) % (1000 * 60)) / 1000);
+  const outTimeSec = Math.trunc(outTime.getTime() / 1000);
+  const leftTime = outTimeSec - todaySec;
+  const hour = Math.floor(leftTime / (60 * 60));
+  const min = Math.floor((leftTime % (60 * 60)) / 60);
+  const sec = Math.floor((leftTime % (60 * 60)) % 60);
   const hours = hour < 10 ? `0${hour}` : hour;
   const minutes = min < 10 ? `0${min}` : min;
   const seconds = sec < 10 ? `0${sec}` : sec;
@@ -55,7 +57,7 @@ function timerMaker() {
 }
 
 //타임세일
-const timeSaleMake = function () {
+const timeSaleMake = () => {
   axios
     .get("../data/timesale.json")
     .then((res) => {
@@ -70,11 +72,11 @@ const timeSaleMake = function () {
       let tempPrice = "";
       tempImg = `
             <div class="imgBox">
-              <img src="../images/main/${item.img}" alt="${item.title}" />
+              <img src="${imgPath}${item.img}" alt="${item.title}" />
             </div>`;
       tempTxt = `
-              <h1 class="title">${item.title}</h1>
-              <p class="desc">${item.desc}</p>`;
+              <a href =""><h1 class="title">${item.title}</h1>
+              <p class="desc">${item.desc}</p></a>`;
       tempPrice = `
             <span class="percent"><strong>${percentage}</strong>%</span><span class="originalPrice">${originalPrice}원</span><span class="salePrice"><strong>${salePrice}</strong>원</span>`;
       saleImgBox.innerHTML = tempImg;
@@ -86,5 +88,145 @@ const timeSaleMake = function () {
     });
 };
 
+//newBest, promotion 생성함수
+const contentsMaker = (title, area) => {
+  axios
+    .get(`../data/${title}.json`)
+    .then((res) => {
+      const data = res.data;
+      const imgPath = data.imgPath;
+      const items = data.items;
+      let tempHtml = "";
+      items.forEach((item, idx) => {
+        if (item.originalPrice > 0) {
+          const percentage = 100 - Math.floor((item.salePrice / item.originalPrice) * 100);
+          const originalPrice = item.originalPrice.toLocaleString("ko-KR");
+          const salePrice = item.salePrice.toLocaleString("ko-KR");
+          tempHtml += `
+        <li class="add purchaseOn">
+        <a href="" class="${title}ImgBox">
+          <div class="imgBox">
+            <img src="${imgPath}${item.img}" alt="${item.title}" />
+          </div>
+        </a>
+        <div class="txtBox">
+          <a href="">
+            <h1 class="title">${item.title}</h1>
+            <p class="desc">${item.desc}</p>
+          </a>
+        </div>
+        <div class="priceBox">
+          <span class="percent"><strong>${percentage}</strong>%</span><span class="originalPrice">${originalPrice}원</span><span class="salePrice"><strong>${salePrice}</strong>원</span>
+        </div>
+        <div class="purchaseBox">
+          <ul>
+            <li>
+              <button><img src="../images/main/heat_btn.png" alt="찜하기" /></button>
+            </li>
+            <li>
+              <button><img src="../images/main/cart_btn.png" alt="장바구니에 넣기" /></button>
+            </li>
+          </ul>
+          <button class="card"><span class="material-icons"> credit_card </span>&nbsp;<span>바로구매</span></button>
+        </div>
+      </li>
+        `;
+        } else {
+          tempHtml += `
+        <li class="add purchaseOn">
+        <a href="" class="newBestImgBox">
+          <div class="imgBox">
+            <img src="${imgPath}${item.img}" alt="${item.title}" />
+          </div>
+        </a>
+        <div class="txtBox">
+          <a href="">
+            <h1 class="title">${item.title}</h1>
+            <p class="desc">${item.desc}</p>
+          </a>
+        </div>
+        <div class="priceBox">
+          <span class="salePrice center"><strong>${item.salePrice}</strong>원</span>
+        </div>
+        <div class="purchaseBox">
+          <ul>
+            <li>
+              <button><img src="../images/main/heat_btn.png" alt="찜하기" /></button>
+            </li>
+            <li>
+              <button><img src="../images/main/cart_btn.png" alt="장바구니에 넣기" /></button>
+            </li>
+          </ul>
+          <button class="card"><span class="material-icons"> credit_card </span>&nbsp;<span>바로구매</span></button>
+        </div>
+      </li>
+        `;
+        }
+        area.innerHTML = tempHtml;
+        if (data.swiper === true) {
+          const swiperList = document.querySelectorAll("#newBest .list .add");
+          swiperList.forEach((li) => {
+            li.classList.add("swiper-slide");
+          });
+        }
+      });
+    })
+    .then(() => {
+      const newBestSwiper = new Swiper("#newBest", {
+        speed: 1000,
+        loop: true,
+        slidesPerView: 5,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: "#newBest .next",
+          prevEl: "#newBest .prev",
+        },
+        // Responsive breakpoints
+        // breakpoints: {
+        //   // when window width is >= 320px
+        //   320: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20
+        //   },
+        //   // when window width is >= 480px
+        //   480: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 30
+        //   },
+        //   // when window width is >= 640px
+        //   640: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40
+        //   }
+        // }
+      });
+    })
+    .then(() => {
+      const filterWrap = () => {
+        const listEl = document.querySelectorAll(`#contents .item .add`);
+        const listElArr = [...listEl];
+        listEl.forEach((item) => {
+          item.addEventListener("mouseenter", () => {
+            listElArr.forEach((item02) => {
+              item02.classList.add("filter");
+            });
+            item.classList.remove("filter");
+          });
+          item.addEventListener("mouseleave", () => {
+            listElArr.forEach((item03) => {
+              item03.classList.remove("filter");
+            });
+          });
+        });
+      };
+      filterWrap();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 setInterval(timerMaker, 1000);
+contentsMaker("newBest", newBestArea);
+contentsMaker("promotion", promotionArea);
 timeSaleMake();
